8aa86f7a508241880f3c02de0a19220adcb5858b
Tantatorn <timefear007@gmail.com>
2023-01-29 23:04:03 +0700

    feat: add margin top to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ export default function Home() {
   return (
     <main
       className={css`
-        background: #150a07;
         width: 100%;
         height: 100vh;
+        color: white;
+        background-color: #150a07;
       `}
     >
       <Hero />
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,21 +4,25 @@ import { css } from "@emotion/react";
 const Hero = () => {
   return (
     <Center height="100%">
-      <Stack spacing={6}>
-        <Heading as="h1" size="xl">
-          Interplanetery Risk Management Marketplace
-        </Heading>
-        <Heading as="h2" size="md" textAlign="center">
-          Purchase protection and secure your file
-          <br />
-          Vice versa, Get rewarded for providing protection
-        </Heading>
+      <Stack spacing={14}>
+        <Stack spacing={6}>
+          <Heading as="h1" size="xl">
+            Interplanetery Risk Management Marketplace
+          </Heading>
+          <Heading as="h2" size="md" textAlign="center">
+            Purchase protection and secure your file
+            <br />
+            Vice versa, Get rewarded for providing protection
+          </Heading>
+        </Stack>
         <Button
           css={css`
-            width: 10rem;
+            width: 12.4rem;
             align-self: center;
             background: #466164;
+            margin-top: 5rem;
           `}
+          size="lg"
         >
           Launch App
         </Button>
