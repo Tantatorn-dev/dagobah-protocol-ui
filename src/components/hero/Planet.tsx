e3c663471670c686c4412e5f6fd3bb7e94b3fd5c
Tantatorn <timefear007@gmail.com>
2023-01-30 20:47:35 +0700

    feat: add planet texture

--- src/components/hero/Planet.tsx
+++ src/components/hero/Planet.tsx
@@ -1,5 +1,23 @@
-import { css } from "@emotion/css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
+import { useRef } from "react";
+
+const PlanetMesh = () => {
+  const meshRef = useRef<any>();
+  const texture = useTexture("/img/planet.png");
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    meshRef.current.rotation.y = a/3;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <sphereGeometry args={[2.6, 32, 32]} />
+      <meshBasicMaterial color="0xffff00" map={texture} />
+    </mesh>
+  );
+};
 
 const Planet = () => {
   return (
@@ -7,10 +25,7 @@ const Planet = () => {
       <mesh>
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <sphereGeometry args={[2.6, 32, 32]} />
-          <meshBasicMaterial color="0xffff00" />
-        </mesh>
+        <PlanetMesh />
       </mesh>
     </Canvas>
   );
