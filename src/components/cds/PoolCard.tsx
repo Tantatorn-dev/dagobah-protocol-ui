8d96bb720d0df4bd4fe5fefbd86e645bcd18eee8
Tantatorn <timefear007@gmail.com>
2023-02-05 08:08:21 +0700

    chore: refactor StakeInput

--- src/components/cds/PoolCard.tsx
+++ src/components/cds/PoolCard.tsx
@@ -1,28 +1,15 @@
 "use client";
-import { zondaxFetcher } from "@/lib/fetcher";
-import { convertBalance } from "@/lib/util";
 import {
-  Button,
-  ButtonGroup,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
   Heading,
   HStack,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
 } from "@chakra-ui/react";
 import { css } from "@emotion/react";
 import { DonutChart, Legend } from "@tremor/react";
-import { useEthers, useSendTransaction } from "@usedapp/core";
-import { ethers } from "ethers";
-import { useMemo, useState } from "react";
-import useSWR from "swr";
-import { mockPoolAddr } from "./PoolList";
+import StakeInput from "./StakeInput";
 
 const valueFormatter = (number: number) =>
   `${Intl.NumberFormat("us").format(number).toString()} TFIL`;
@@ -34,21 +21,6 @@ type Props = {
 };
 
 const PoolCard: React.FC<Props> = ({ name, myValue, other }) => {
-  const { account } = useEthers();
-  const { data } = useSWR(`/account/balance/${account}`, zondaxFetcher);
-  const myBalance = useMemo(() => {
-    return data ? convertBalance(data.balances[0].value) : 0;
-  }, [data]);
-  const { sendTransaction } = useSendTransaction();
-  const [sendValue, setSendValue] = useState(0);
-
-  const onStake = () => {
-    sendTransaction({
-      value: ethers.utils.parseEther(sendValue.toString()),
-      to: mockPoolAddr,
-    });
-  };
-
   const chartData = [
     {
       name: "Other",
@@ -87,24 +59,7 @@ const PoolCard: React.FC<Props> = ({ name, myValue, other }) => {
         </HStack>
       </CardBody>
       <CardFooter>
-        <ButtonGroup spacing="2" width="100%" justifyContent="flex-end">
-          <NumberInput
-            max={myBalance}
-            keepWithinRange={false}
-            clampValueOnBlur={false}
-            value={sendValue}
-            onChange={(value) => setSendValue(parseFloat(value))}
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-          <Button onClick={onStake} variant="solid" colorScheme="blue">
-            Stake
-          </Button>
-        </ButtonGroup>
+        <StakeInput />
       </CardFooter>
     </Card>
   );
---
+++ src/components/cds/StakeInput.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { zondaxFetcher } from "@/lib/fetcher";
+import { convertBalance } from "@/lib/util";
+import {
+  Button,
+  ButtonGroup,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+} from "@chakra-ui/react";
+import { useEthers, useSendTransaction } from "@usedapp/core";
+import { ethers } from "ethers";
+import { useMemo, useState } from "react";
+import useSWR from "swr";
+import { mockPoolAddr } from "./PoolList";
+
+const StakeInput = () => {
+  const { account } = useEthers();
+  const { data } = useSWR(`/account/balance/${account}`, zondaxFetcher);
+  const myBalance = useMemo(() => {
+    return data ? convertBalance(data.balances[0].value) : 0;
+  }, [data]);
+  const { sendTransaction } = useSendTransaction();
+  const [sendValue, setSendValue] = useState(0);
+
+  const onStake = () => {
+    sendTransaction({
+      value: ethers.utils.parseEther(sendValue.toString()),
+      to: mockPoolAddr,
+    });
+  };
+
+
+  return (
+    <ButtonGroup spacing="2" width="100%" justifyContent="flex-end">
+      <NumberInput
+        max={myBalance}
+        keepWithinRange={false}
+        clampValueOnBlur={false}
+        value={sendValue}
+        onChange={(value) => setSendValue(parseFloat(value))}
+      >
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      <Button onClick={onStake} variant="solid" colorScheme="blue">
+        Stake
+      </Button>
+    </ButtonGroup>
+  );
+};
+
+export default StakeInput;
