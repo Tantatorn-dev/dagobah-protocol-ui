8adae258248c3a29ced240c702ae52780c2df004
Tantatorn <timefear007@gmail.com>
2023-02-05 20:00:30 +0700

    fix: add mode cors

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,7 +2,10 @@ import { FILFOX_URL, ZONDAX_URL } from "./const";
 
 export const zondaxFetcher = (url: string) =>
   fetch(`${ZONDAX_URL}${url}`, {
-    headers: { Authorization: "Bearer " + process.env.NEXT_PUBLIC_ZONDAX_KEY },
+    mode: "cors",
+    headers: {
+      Authorization: "Bearer " + process.env.NEXT_PUBLIC_ZONDAX_KEY,
+    },
   }).then((res) => res.json());
 
 export const filfoxFetcher = (url: string) =>
